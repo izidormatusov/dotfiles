03248901fc23131ce2ca50ef959bb70e7a64a2f3
Izidor Matušov <izidor.matusov@gmail.com>
2015-02-22 19:49:57 +0100

    Create bytes.py
    Useful script for ipython console

---
+++ ipython/profile_default/startup/bytes.py
@@ -0,0 +1,14 @@
+def bytes(b):
+  """ Print bytes in a humanized way """
+  def humanize(b, base, suffices=[]):
+    bb = int(b)
+    for suffix in suffices:
+      if bb < base:
+        break
+      bb /= float(base)
+    return "%.2f %s" % (bb, suffix)
+
+  print "Base 1024: ", humanize(
+      b, 1024, ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB'])
+  print "Base 1000: ", humanize(
+      b, 1000, ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB'])
