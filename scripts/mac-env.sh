03a4304898684183b9cda44be1cbe217e2122afa
Izidor Matušov <izidor.matusov@gmail.com>
2025-05-18 23:09:44 +0200

    Update mac-env.sh

--- scripts/mac-env.sh
+++ scripts/mac-env.sh
@@ -1,81 +1,33 @@
-set -e
+#!/bin/bash
 
-# Only on Mac
-if [ `uname -s` = "Darwin" ]
-then
+set -euo pipefail
 
 heading() {
   echo -e "\033[33;1m$@\033[0m"
 }
 
-# Set computer hostname if needed
-setHostname() {
-    sudo scutil --set ComputerName "$1"
-    sudo scutil --set HostName "$1"
-    sudo scutil --set LocalHostName "$1"
-    sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$1"
-}
+# Only on Mac
+if [ `uname -s` = "Darwin" ]
+then
 
-PlistDefine() {
-    local name="$1"
-    local type="$2"
-    local value="$3"
-    local file="$4"
-    if ! PlistBuddy -c "print $name" "$file" > /dev/null 2>&1
-    then
-        PlistBuddy -c "Add $name $type $value" "$file"
-    else
-        PlistBuddy -c "Set $name $value" "$file"
-    fi
-}
-
+osascript -e 'tell application "System Preferences" to quit' || echo "Could not close system preferences"
 
 heading "Setting up mac environment"
-
-# Use Dark Mojave theme
+# Use Dark theme
 defaults write -g AppleInterfaceStyle Dark
-
 # Show scroll bars when scrolling
 defaults write -g AppleShowScrollBars Automatic
-
-# Do not automatically switch to a space with the open windows for the application
-defaults write -g AppleSpacesSwitchOnActivate -bool false
-
-
-# Fix the mouse scrolling
-defaults write -g com.apple.swipescrolldirection -bool false
-
-# Save screenshots into Downloads folder
-defaults write com.apple.screencapture location ~/Downloads
-
-# Remove items from the menubar
-defaults write com.apple.systemuiserver "NSStatusItem Visible com.apple.menuextra.appleuser" -bool false
-defaults write com.apple.systemuiserver "NSStatusItem Visible com.apple.menuextra.bluetooth" -bool false
-
-defaults write com.apple.Siri StatusMenuVisible -bool false
-defaults write com.apple.Spotlight "NSStatusItem Visible Item-0" -bool false
-defaults write com.apple.airplay showInMenuBarIfPresent -bool false
-
-defaults write com.apple.controlcenter "NSStatusItem Visible Battery" -bool true
-defaults write com.apple.controlcenter "NSStatusItem Visible Clock" -bool true
-defaults write com.apple.controlcenter "NSStatusItem Visible DoNotDisturb" -bool true
-defaults write com.apple.controlcenter "NSStatusItem Visible Sound" -bool true
-
-defaults write com.apple.controlcenter "NSStatusItem Visible Bluetooth" -bool false
-defaults write com.apple.controlcenter "NSStatusItem Visible KeyboardBrightness" -bool false
-defaults write com.apple.controlcenter "NSStatusItem Visible NowPlaying" -bool false
-defaults write com.apple.controlcenter "NSStatusItem Visible UserSwitcher" -bool false
-defaults write com.apple.controlcenter "NSStatusItem Visible WiFi" -bool false
 
 # Disable the "Are you sure you want to open this application?" dialog
 defaults write com.apple.LaunchServices LSQuarantine -bool false
 
-# Set screensaver to Flurry
-# Preference is stored in ByHost folder so -currentHost is required
-defaults -currentHost write com.apple.screensaver moduleDict -dict \
-    moduleName Flurry path "/System/Library/Screen Savers/Flurry.saver" type 0
-# Show clock on the screensaver
-defaults -currentHost write com.apple.screensaver showClock -int 1
+heading "Sound"
+# Don't play user interface sound effects
+defaults write -g com.apple.sound.uiaudio.enabled -bool false
+# Don't play feedback when volume is changed
+defaults write -g com.apple.sound.beep.feedback -bool false
+# Lower volume of the feedback
+defaults write -g com.apple.sound.beep.volume -float 0.2
 
 # Disable chime noise when plugging in power charger
 # (source: https://apple.stackexchange.com/a/309947/327859)
@@ -84,18 +36,26 @@ defaults -currentHost write com.apple.screensaver showClock -int 1
 # laptop lid is closed
 defaults write com.apple.PowerChime ChimeOnNoHardware -bool true
 
-# Keyboard mappings (CapsLock -> ESC) are per keyboard.
-# The mapping includes the vendor id and product id of the keyboard which
-# obviously varies for each type of the keyboard. Instead of hardcoding the
-# values here, just check if there are any existing ones otherwise instruct
-# user to change it.
-# More context: https://apple.stackexchange.com/q/4813
-if ! defaults -currentHost read -g | grep -q com.apple.keyboard.modifiermapping
-then
-    heading "Please map CapsLock to ESC in System Preferences -> Keyboard > Modifier keys"
-    exit 1
-fi
+# Disable startup noise
+sudo nvram StartupMute=%01
 
+heading "Background sounds"
+# https://support.apple.com/en-me/guide/mac-help/mchl3061cdc6/mac
+defaults write com.apple.ComfortSounds relativeVolume -float 0.18
+# Stop after the computer is locked
+defaults write com.apple.ComfortSounds stopsOnLock -bool true
+
+heading "Mouse / trackpad"
+# Fix scroll direction
+defaults write -g com.apple.swipescrolldirection -bool false
+# Increase mouse speed
+defaults write -g com.apple.mouse.scaling -float 2.5
+# Increase trackpad speed
+defaults write -g com.apple.trackpad.scaling -float 1
+# Tap to click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+
+heading "Keyboard"
 # Disable automatic text corrections
 defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
 defaults write -g WebAutomaticSpellingCorrectionEnabled -bool false
@@ -109,39 +69,35 @@ defaults write -g NSAutomaticQuoteSubstitutionEnabled -bool false
 defaults write -g AppleKeyboardUIMode -int 2
 
 # Set fast key repeat rate
-defaults write -g KeyRepeat -int 6
+defaults write -g KeyRepeat -int 2
 defaults write -g InitialKeyRepeat -int 15
 
-## Trackpad
-# Allow tap to click
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+# Disable the popup menu for accents
+defaults write -g ApplePressAndHoldEnabled -bool false
 
-# Disable UI sound effects
-defaults write -g com.apple.sound.uiaudio.enabled -bool false
+heading "Keyboard shortcuts"
+# Disable spotlight with ⌘ + Space (Alfred should remap to it)
+# defaults command is not powerful enough to update this configuration.
+# PlistBuddy first removes the key entry and then adds it back disabled.
+/usr/libexec/PlistBuddy ~/Library/Preferences/com.apple.symbolichotkeys.plist \
+  -c "Delete :AppleSymbolicHotKeys:64" \
+  -c "Add :AppleSymbolicHotKeys:64:enabled bool false" \
+  -c "Add :AppleSymbolicHotKeys:64:value:parameters array" \
+  -c "Add :AppleSymbolicHotKeys:64:value:parameters: integer 65535" \
+  -c "Add :AppleSymbolicHotKeys:64:value:parameters: integer 49" \
+  -c "Add :AppleSymbolicHotKeys:64:value:parameters: integer 1048576" \
+  -c "Add :AppleSymbolicHotKeys:64:type string standard"
 
-# Disable feedback when volume is changed
-defaults write -g com.apple.sound.beep.feedback -bool false
+heading "Dock and windows"
+# Disable the tiles in the window manager
+defaults write com.apple.WindowManager EnableTiledWindowMargins -bool false
+# Workspace uses all available monitor (not workspace per monitor)
+defaults write com.apple.spaces spans-displays -bool true
+# Do not automatically switch to a space with the open windows for the application
+defaults write -g AppleSpacesSwitchOnActivate -bool false
 
-# Show volume in menubar
-defaults write com.apple.systemuiserver "NSStatusItem Visible com.apple.menuextra.volume" -bool true
+defaults write com.apple.WindowManager EnableStandardClickToShowDesktop -bool False
 
-# Set the format of clock to something like "7 Feb 22:57"
-defaults write com.apple.menuextra.clock DateFormat "d MMM HH:mm"
-
-# Show battery percentage in the menu
-defaults write com.apple.menuextra.battery ShowPercent -string "YES"
-
-# Setting the wallpaper
-WALLPAPER="$HOME/Documents/wallpaper.jpg"
-if [ -f "$WALLPAPER" ]
-then
-    heading "Setting the wallpaper"
-    osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$WALLPAPER\""
-else
-    echo "No wallpaper, skipping"
-fi
-
-heading "Dock"
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
 
@@ -153,6 +109,9 @@ defaults write com.apple.dock tilesize -int 31
 
 # Make the icons smaller
 defaults write com.apple.dock "show-recents" -int 0
+
+# Don't automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
 
 heading "Hot corners"
 # Possible values:
@@ -173,19 +132,56 @@ defaults write com.apple.dock wvous-tl-modifier -int 0
 defaults write com.apple.dock wvous-tr-corner -int 12
 defaults write com.apple.dock wvous-tr-modifier -int 0
 # Bottom left: Put display to sleep
-defaults write com.apple.dock wvous-bl-corner -int 10
+defaults write com.apple.dock wvous-bl-corner -int 0
 defaults write com.apple.dock wvous-bl-modifier -int 0
 # Bottom right: no-op
 defaults write com.apple.dock wvous-br-corner -int 0
 defaults write com.apple.dock wvous-br-modifier -int 0
 
-# Don't automatically rearrange Spaces based on most recent use
-defaults write com.apple.dock mru-spaces -bool false
+heading "Menubar"
+# Remove items from the menubar
+defaults write com.apple.systemuiserver "NSStatusItem Visible com.apple.menuextra.appleuser" -bool false
+defaults write com.apple.systemuiserver "NSStatusItem Visible com.apple.menuextra.bluetooth" -bool false
+# Show volume in menubar
+defaults write com.apple.systemuiserver "NSStatusItem Visible com.apple.menuextra.volume" -bool true
 
-killall -HUP Dock
+# Set the format of clock to something like "7 Feb 22:57"
+defaults write com.apple.menuextra.clock DateFormat "d MMM HH:mm"
+
+defaults write com.apple.Siri StatusMenuVisible -bool false
+
+defaults write com.apple.Spotlight "NSStatusItem Visible Item-0" -bool false
+defaults -currentHost write com.apple.Spotlight MenuItemHidden -bool true
+# Don't share spotlight queries
+defaults write com.apple.assistant.support "Search Queries Data Sharing Status" -int 2
+
+defaults write com.apple.airplay showInMenuBarIfPresent -bool false
+
+defaults write com.apple.controlcenter "NSStatusItem Visible Battery" -bool true
+defaults write com.apple.controlcenter "NSStatusItem Visible Clock" -bool true
+defaults write com.apple.controlcenter "NSStatusItem Visible DoNotDisturb" -bool true
+defaults write com.apple.controlcenter "NSStatusItem Visible FocusModes" -bool true
+defaults write com.apple.controlcenter "NSStatusItem Visible Sound" -bool true
+
+defaults write com.apple.controlcenter "NSStatusItem Visible Bluetooth" -bool false
+defaults write com.apple.controlcenter "NSStatusItem Visible KeyboardBrightness" -bool false
+defaults write com.apple.controlcenter "NSStatusItem Visible NowPlaying" -bool false
+defaults write com.apple.controlcenter "NSStatusItem Visible UserSwitcher" -bool false
+defaults write com.apple.controlcenter "NSStatusItem Visible WiFi" -bool false
+
+# Display battery in menu bar with percentage and energy mode
+defaults -currentHost write com.apple.controlcenter Battery -int 3
+defaults -currentHost write com.apple.controlcenter BatteryShowPercentage -bool true
+defaults -currentHost write com.apple.controlcenter BatteryShowEnergyMode -bool true
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+
+heading "Screenshots"
+# Save screenshots into Downloads folder
+defaults write com.apple.screencapture location ~/Documents/screenshots
+# Disable shadows around the window screenshots
+defaults write com.apple.screencapture disable-shadow -bool false
 
 heading "Finder"
-
 # Show `Library` folder
 chflags nohidden ~/Library/
 
@@ -201,6 +197,7 @@ defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
 defaults write com.apple.finder ShowRecentTags -bool false
 
 # By default open Downloads folder
+defaults write com.apple.finder NewWindowTarget -string "PfLo"
 defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/Downloads"
 
 # When searching, search the current folder
@@ -224,10 +221,6 @@ defaults write -g AppleShowAllExtensions -bool true
 
 # Show the full path in title bar
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
-
-# Writing of .DS_Store files on network or USB volumes
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
 defaults write com.apple.finder "NSToolbar Configuration Browser" '{
     "TB Default Item Identifiers" = (
@@ -261,46 +254,58 @@ defaults write com.apple.finder "NSToolbar Configuration Browser" '{
 
 killall Finder
 
-heading "Enable night shift"
-USER_UID="$(dscl . -read ~ GeneratedUID | sed 's/GeneratedUID: //')"
-NIGHTSHIFT_FILE="/var/root/Library/Preferences/com.apple.CoreBrightness.plist"
-NIGHTSHIFT_CONFIG='{
-  CBBlueLightReductionCCTTargetRaw = 3089.0;
-  CBBlueReductionStatus = {
-    AutoBlueReductionEnabled = 1;
-    BlueLightReductionAlgoOverride = 0;
-    BlueLightReductionDisableScheduleAlertCounter = 3;
-    BlueLightReductionSchedule = {
-      DayStartHour = 6;
-      DayStartMinute = 0;
-      NightStartHour = 19;
-      NightStartMinute = 0;
-    };
-    BlueReductionAvailable = 1;
-    BlueReductionEnabled = 0;
-    BlueReductionMode = 2;
-    BlueReductionSunScheduleAllowed = 0;
-    Version = 1;
+heading "Night shift"
+NIGHT_SHIFT='{
+  CBBlueLightReductionCCTTargetRaw = "3034";
+  CBBlueReductionStatus =         {
+      AutoBlueReductionEnabled = 1;
+      BlueLightReductionAlgoOverride = 0;
+      BlueLightReductionDisableScheduleAlertCounter = 3;
+      BlueLightReductionSchedule =             {
+          DayStartHour = 6;
+          DayStartMinute = 30;
+          NightStartHour = 19;
+          NightStartMinute = 30;
+      };
+      BlueReductionAvailable = 1;
+      BlueReductionEnabled = 0;
+      BlueReductionMode = 2;
+      BlueReductionSunScheduleAllowed = 1;
+      Version = 1;
   };
 }'
 
-sudo defaults write $NIGHTSHIFT_FILE "CBUser-0" "$NIGHTSHIFT_CONFIG"
-sudo defaults write $NIGHTSHIFT_FILE "CBUser-${USER_UID}" "$NIGHTSHIFT_CONFIG"
+CORE_BRIGHTNESS="/var/root/Library/Preferences/com.apple.CoreBrightness.plist"
+CBUser="CBUser-$(dscl . -read ~ GeneratedUID | sed 's/GeneratedUID: //')"
+sudo defaults write $CORE_BRIGHTNESS "CBUser-0" "$NIGHT_SHIFT"
+sudo defaults write $CORE_BRIGHTNESS "$CBUser"  "$NIGHT_SHIFT"
 
-heading "Evernote"
-# Show note counts in sidebar
-defaults write com.evernote.Evernote ENLeftNavShowsNoteCounts -int 1
+heading "Automatic sleep"
 
-# Start evernote on the login
-defaults write com.evernote.Evernote runAtLogin -int 1
+# Show the currently scheduled things
+#   sudo pmset -g sched
+# Documentation
+#   man pmset
+# See https://iboysoft.com/news/missing-energy-saver-schedule-ventura.html
+# Schedule the time to sleep at 10:01pm
+sudo pmset repeat cancel && \
+  sudo pmset repeat sleep MTWRFSU 22:01:00
 
-# Do not play sound after clip
-defaults write com.evernote.Evernote ENPlaySoundAfterClip -int 0
+heading "pinentry-mac"
+# Do not force me to store password in the keychain
+defaults write org.gpgtools.common DisableKeychain -bool yes
 
-if ! lpstat -p > /dev/null
-then
-    heading "No printer. Please add one"
-    exit 1
-fi
+heading "Spotify"
+mkdir -p ~/Library/Application\ Support/Spotify
+touch ~/Library/Application\ Support/Spotify/prefs
+echo 'app.autostart-mode="off"' >> ~/Library/Application\ Support/Spotify/prefs
+echo 'app.autostart-banner-seen=true' >> ~/Library/Application\ Support/Spotify/prefs
+
+heading "Restart apps to pick up the new settings"
+set -x
+killall SystemUIServer || true
+killall -HUP Dock || true
+killall Finder || true
+killall cfprefsd || true
 
 fi
