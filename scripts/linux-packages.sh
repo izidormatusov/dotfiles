23207a9e3ddfc400b2d26dbceb382f8e7e4ce807
Izidor Matušov <izidor.matusov@gmail.com>
2025-05-18 23:09:36 +0200

    Update linux-packages.sh

--- scripts/linux-packages.sh
+++ scripts/linux-packages.sh
@@ -1,43 +1,62 @@
-set -e
-if [ `uname -s` = "Linux" ]
-then
+#!/bin/bash
+
+set -euo pipefail
 
 heading() {
   echo -e "\033[33;1m$@\033[0m"
 }
 
-# List installed packages on the system
-# $ apt --installed list | grep -v automatic
+if [ $(uname -s) = "Linux" ]; then
+  # List installed packages on the system
+  # $ apt --installed list | grep -v automatic
 
-heading "Linux packages: apt update"
-sudo apt update
+  heading "Linux packages: apt update"
+  sudo apt update
 
-heading "Linux packages: apt install"
-sudo apt install --assume-yes \
-    binwalk \
-    colordiff \
-    d-feet \
-    entr \
-    exiftool \
-    fdupes \
-    fzf \
-    gimp \
-    graphviz \
-    htop \
-    inkscape \
-    iotop \
-    ipython3 \
-    irssi \
-    jq \
-    nmap \
-    pandoc \
-    pdftk \
-    ripgrep \
-    spotify-client \
-    tzwatch \
-    vlc \
-    wireshark \
-    wmctrl \
-    youtube-dl
+  heading "Linux packages: apt install"
 
+  packages=(
+      bat
+      binwalk
+      btop
+      colordiff
+      d-feet
+      entr
+      exiftool
+      fd-find
+      fdupes
+      flatpak
+      fzf
+      gimp
+      graphviz
+      htop
+      inkscape
+      iotop
+      ipython3
+      irssi
+      jq
+      ncdu
+      nmap
+      pandoc
+      pdftk
+      qrencode
+      ripgrep
+      rofi
+      spotify-client
+      sqlite3
+      tzwatch
+      vlc
+      wireshark
+      wmctrl
+      youtube-dl
+  )
+
+  sudo apt install --assume-yes ${packages[@]}
+
+  heading "Linux packages: flatpak"
+  flatpak remote-add --if-not-exists \
+    flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+  # Pomodoro timer
+  flatpak install flathub org.gnome.Solanum
 fi
